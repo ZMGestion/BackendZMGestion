7258c94cdf967895cff4c2040f9867d9cbbb8eaa
lchoua <loikchoua4@gmail.com>
2020-12-08 20:52:22 -0300

    agrego lista de precios

--- internal/models/reportesService.go
+++ internal/models/reportesService.go
@@ -33,3 +33,26 @@ func (rs *ReportesService) Stock(token string) ([]map[string]interface{}, error)
 
 	return response, err
 }
+
+//ListaPrecios Funcion que permite generar la lista de precios
+func (rs *ReportesService) ListaPrecios(token string) ([]map[string]interface{}, error) {
+	usuariosEjecuta := structs.Usuarios{
+		Token: token,
+	}
+
+	param := map[string]interface{}{
+		"UsuariosEjecuta": usuariosEjecuta,
+	}
+
+	out, err := rs.DbHandler.CallSP("zsp_reportes_listaPrecios", param)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []map[string]interface{}
+
+	err = json.Unmarshal(*out, &response)
+
+	return response, err
+}
